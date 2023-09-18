901ad253d581cdce41b0121c00be4dbf2bd96ad9
Dipawesh <dipawesh.iitp@gmail.com>
2023-09-19 03:08:53 +0530

    Time: 20 ms (27.22%) | Memory: 13.2 MB (50.82%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        j = 0
+        for num in nums:
+            if num != val:
+                nums[j] = num
+                j += 1
+        return j
+
+        

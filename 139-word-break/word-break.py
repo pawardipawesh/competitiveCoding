10c3a6040669ffd220e8ac2eafbdd56b9a75d5eb
Dipawesh <dipawesh.iitp@gmail.com>
2023-09-20 21:03:02 +0530

    Time: 190 ms (5.29%) | Memory: 16.2 MB (99.71%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,12 +1,10 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         def is_segmentable(s, word_dict):
-            # iss_l = []
             for word in word_dict:
                 if word in s:
                     rs = [ w for w in s.split(word) if w.strip()]
                     if len(rs) == 0:
-                        # print('rs: ', rs)
                         return True
                     
                     allrp_segmentable = True
@@ -14,15 +12,10 @@ class Solution:
                         if not is_segmentable(w, word_dict):
                             allrp_segmentable = False
                             break
-                        # issp_l.append(is_sp)
                     
-                    # iss_l.append(all(issp_l))
-                    # if all(issp_l):
-                    #     return True
                     if allrp_segmentable:
                         return True
             return False
-            # return any(iss_l)
         return is_segmentable(s, wordDict)
         
         

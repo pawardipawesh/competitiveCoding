3dab019fdeca8520a5ac8962d9aa4de5459e134e
Dipawesh <dipawesh.iitp@gmail.com>
2023-09-25 00:03:13 +0530

    Time: 59 ms (34.64%) | Memory: 16.6 MB (99.67%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,31 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ls = len(s)
+        lt = len(t)
+        if ls != lt:
+            return False
+        
+        sc = {}
+        for char in s:
+            if char not in sc:
+                sc[char] = 1
+            else:
+                sc[char] += 1
+        
+        for char in t:
+            if char not in sc:
+                return False
+            
+            if char in sc:
+                sc[char] -= 1
+
+                if sc[char] < 0:
+                    return False
+        
+        sum_sc = sum(sc.values())
+        if sum_sc > 0 or sum_sc < 0:
+            return False
+        
+        return True
+        

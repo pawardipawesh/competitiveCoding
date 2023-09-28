db032af5d4744fe223a212b4f4e30cc81d9480be
Dipawesh <dipawesh.iitp@gmail.com>
2023-09-28 07:53:15 +0530

    Time: 40 ms (85.70%) | Memory: 16.3 MB (93.04%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -2,10 +2,7 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         ls = len(s)
 
-        mid = len(s)//2
-
-        i = 0
-        for j in range(mid, i-1, -1):
+        for j in range(len(s)//2, -1, -1):
 
             lss = j + 1
             if ls % lss == 0:
@@ -14,7 +11,7 @@ class Solution:
                 if m == 1:
                     continue
 
-                ss = s[i:j+1]
+                ss = s[0:j+1]
 
                 if s == ''.join([ss] * m):
                     return True
